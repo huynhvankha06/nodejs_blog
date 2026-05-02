8a08ca8f60c17fe338c997e7157ce3817417f1e7
Huynh Van Kha <huynhvankha100306@gmail.com>
2026-05-02 07:47:41 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
 // app.use(morgan("combined"));
 
 //template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-    partialsDir: path.join(__dirname, "resources/views/partials"),
-  }),
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        partialsDir: path.join(__dirname, 'resources/views/partials'),
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set(
+    'view engine',
+
+    'hbs',
+);
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
